a7acc759ccce0a2525ad79871308dc41db8702e0
Yuideg <misganewendeg879@gmail.com>
2021-05-07 00:00:57 +0300

    commit navbar and notfound components

---
+++ src/NotFound.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom"
+
+const NotFound = () => {
+    return (
+        <div className="not-found">
+            <h2>Sorry</h2>
+            <p>That page is not found</p>
+            <Link to="/">back to home page...</Link>
+
+        </div>
+     );
+}
+ 
+export default NotFound;
